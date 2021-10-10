d57e5f5a59b380bf628312dff3fc53e51b8c6641
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-10-09 20:52:18 -0500

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Renato Oliveira Fraga on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
